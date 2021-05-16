d7a672a2204c2a78b0c0c6e4801cac465c51b30c
Guillaume Wells <wellsguillaume@gmail.com>
2021-05-16 11:25:43 +0200

    wip

--- clone.py
+++ clone.py
@@ -8,13 +8,10 @@ config = dotenv_values('.env')
 
 def doRequest(payload):
     response = requests.get('https://api.github.com/user/repos', params=payload, auth=(config['USERNAME'],config['TOKEN']))
-    #pprint(response.json)
     for repo in response.json():
-        print(repo['url'])
-        print(repo['language'] if repo['language'] else 'Others', end='') 
-        print('...'+repo['url'])
+        print(repo['language'] if repo['language'] else 'Others', end='\t') 
+        print(repo['ssh_url'])
     return len(response.json())
-
 
 items = doRequest({'page': '0', 'per_page': '100'})
 
--- clone.sh
+++ clone.sh
@@ -1,3 +1,90 @@
 #!/bin/bash
 
-CLONE=$(python clone.py)
+set -e
+
+# ERRORS CODE
+
+E_BADARGS=11
+E_BADLOCATION=12
+
+usage()
+{
+    cat <<UsagePrint
+Usage :
+    ./clone.bash -t target [-v] [-h]
+    ./clone.bash --target target [--verbose] [--help]
+    Clone all your repositories from your github account
+    Verbose : Permit to see what is done at each step
+    Help : Print this help
+UsagePrint
+
+exit $E_BADARGS
+}
+
+close_stderr_stdout()
+{
+    exec 11>&1 12>&2 1>&- 2>&-     
+}
+
+restore_stderr_stdout()
+{
+    exec 11>&1- 12>&2-     
+}
+
+options=$(getopt -a -n "$(basename $0)" -l "target:,help,verbose" -- "t:hvd" "$@")
+
+if [ $? -ne 0 ]; then
+    usage
+fi
+
+eval set --$options
+
+while [ ! -z "$1" ]
+do
+    case "$1" in
+        -t) target="$2"; shift ;;
+        -v) verbose=1 ;;
+        -h) usage ;;
+        --target) target="$2"; shift ;;
+        --help) usage ;;
+        --verbose) verbose=1 ;;
+    esac
+    shift
+done
+
+v=""
+if [[ $verbose ]]; then
+    v="v"
+fi
+
+if [ -z $target ]; then
+    echo "Target path is manadatory"
+    exit $E_BADARGS
+fi
+
+if [ ! -d $target ]; then
+    echo "This is not  a valid location"
+    exit $E_BADLOCATION
+fi
+
+if [[ $verbose -eq 0 ]]; then
+    close_stderr_stdout
+fi
+
+
+while read -r line
+do
+    cd "$target"
+    lang=$( echo "$line" | cut -f1)
+    url=$( echo "$line" | cut -f2)
+    if [ ! -d "$lang" ]; then
+        mkdir $([[ $verbose = 1 ]] && echo "-v") "$lang"
+    fi
+    cd "$lang"
+    git clone "$url"
+done < <(python clone.py)
+
+if [[ $verbose -eq 1 ]]; then
+    restore_stderr_stdout
+fi
+
